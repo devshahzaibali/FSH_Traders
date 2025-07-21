345a34fa8ed91e8c6fae39b8443225d72f4da7c7
devshahzaibali <techzaibx@gmail.com>
2025-07-21 09:34:36 -0700

    Switch to next.config.js and ensure res.cloudinary.com is allowed for Next.js images

---
+++ next.config.js
@@ -0,0 +1,50 @@
+/** @type {import('next').NextConfig} */
+const nextConfig = {
+    images: {
+        remotePatterns: [{
+                protocol: 'https',
+                hostname: 'm.media-amazon.com',
+                port: '',
+                pathname: '/**',
+            },
+            {
+                protocol: 'https',
+                hostname: 'images.unsplash.com',
+                port: '',
+                pathname: '/**',
+            },
+            {
+                protocol: 'https',
+                hostname: 'via.placeholder.com',
+                port: '',
+                pathname: '/**',
+            },
+            {
+                protocol: 'https',
+                hostname: 'picsum.photos',
+                port: '',
+                pathname: '/**',
+            },
+            {
+                protocol: 'https',
+                hostname: 'firebasestorage.googleapis.com',
+                port: '',
+                pathname: '/**',
+            },
+            {
+                protocol: 'https',
+                hostname: 'drive.google.com',
+                port: '',
+                pathname: '/uc',
+            },
+            {
+                protocol: 'https',
+                hostname: 'res.cloudinary.com',
+                port: '',
+                pathname: '/**',
+            },
+        ],
+    },
+};
+
+module.exports = nextConfig;
